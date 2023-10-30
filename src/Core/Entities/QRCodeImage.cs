9e2126aa2d22347f7f6e387cceb44757ea0d4ad6
Mustafa Shakir <mshakir9918@gmail.com>
2023-10-30 16:41:11 +0200

    Create Config

--- src/Core/Entities/QRCodeImage.cs
+++ src/Core/Entities/QRCodeImage.cs
@@ -2,6 +2,10 @@
 {
     public class QRCodeImage
     {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
         public byte[] Image { get; set; }
     }
 }
---
+++ src/External/Configs/QRCodeImageConfig.cs
@@ -0,0 +1,21 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DAL.Configs
+{
+    public class QRCodeImageConfig : IEntityTypeConfiguration<QRCodeImage>
+    {
+        public void Configure(EntityTypeBuilder<QRCodeImage> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Name)
+                .HasMaxLength(200)
+                .IsRequired();
+
+            builder.Property(x => x.Image)
+                .IsRequired();
+        }
+    }
+}
